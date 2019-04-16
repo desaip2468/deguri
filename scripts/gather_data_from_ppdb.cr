ac28e2dea28c38f221deb73d5f4b20477786d29e
pjshwa <pjshwa@gmail.com>
2019-04-16 23:47:31 +0900

    EDIT - ppdb plot 에 연령대별 응답 비율 알 수 있도록 `total` column 추가

--- scripts/gather_data_from_ppdb.cr
+++ scripts/gather_data_from_ppdb.cr
@@ -1,12 +1,13 @@
 require "csv"
 
 m_headers = (1..27).map {|i| fs = "%02d" % i; "M00#{fs}"}
+m_csv_headers = m_headers + %w[total]
 m_counts_by_age = Hash(Int32, Hash(String, Int32)).new
 
 # Fill data after compiler type inference
 (1..6).each do |i|
   m_counts = Hash(String, Int32).new
-  m_headers.each {|m_header| m_counts[m_header] = 0}
+  m_csv_headers.each {|m_csv_header| m_counts[m_csv_header] = 0}
   m_counts_by_age[i] = m_counts
 end
 
@@ -16,6 +17,7 @@ while csv.next
   i += 1
   print "\rrow: #{i}"
   m_h = m_counts_by_age[csv["X0002"].to_i]
+  m_h["total"] += 1
 
   m_headers.each do |m_header|
     m_h[m_header] += 1 if csv[m_header] == "1"
@@ -26,10 +28,11 @@ puts
 puts "Complete!"
 
 generated_csv = CSV.build do |csv|
-  csv.row %w[group] + m_headers
+  csv.row(%w[group] + m_csv_headers)
 
-  m_counts_by_age.each do |group, values_hash|
-    csv.row [group] + m_headers.map {|m_header| values_hash[m_header]}
+  (1..6).each do |group|
+    values_hash = m_counts_by_age[group]
+    csv.row([group] + m_csv_headers.map {|m_csv_header| values_hash[m_csv_header]})
   end
 end
 
--- scripts/plot_ppdb.R
+++ scripts/plot_ppdb.R
@@ -1,13 +1,24 @@
 library(ggplot2)
 library(plyr) # splat
 library(purrr) # map
+library(scales)
 
 source("multiplot.R")
 
-csv <- read.csv("output.csv")
-headers <- c("M0001","M0002","M0003","M0004","M0005","M0006","M0007","M0008","M0009","M0010","M0011","M0012","M0013","M0014","M0015","M0016","M0017","M0018","M0019","M0020","M0021","M0022","M0023","M0024","M0025","M0026","M0027")
-splat(multiplot)(map(headers, function(x){
-  ggplot(csv, aes_string("group", x, fill="group")) +
-    geom_bar(stat = "identity", position = "dodge") +
-    geom_text(aes_string(label = x))
-}), cols=6)
+headers <- c("J0079", "J0080", "J0081")
+colClasses <- c(map(headers, function(x){ "numeric" }), "numeric")
+csv <- read.csv("output.csv", colClasses = colClasses)
+
+splat(multiplot)(
+  map(
+    headers,
+    function(x){
+      ggplot(csv, aes_string("group", x, fill = "group")) +
+        geom_bar(stat = "identity", position = "dodge") +
+        geom_label(aes_string(label = x, fill = 1), colour = "white", fontface = "bold") +
+        geom_label(aes_string(label = paste("percent(", x, "/ total)")), colour = "white", vjust = 1.5)
+        # geom_label(aes(label = paste("from group ", group)), colour = "white", vjust = 2.5)
+    }
+  ),
+  cols = 2
+)
