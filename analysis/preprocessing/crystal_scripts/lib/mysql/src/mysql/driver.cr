a00ade9a83dd0dec7a24c03d273ee8e288090551
pjshwa <pjshwa@gmail.com>
2019-06-13 17:54:37 +0900

    REMOVE - `gh-pages` branch 불필요한 내용 제거

--- analysis/preprocessing/crystal_scripts/lib/mysql/src/mysql/driver.cr
+++
@@ -1,7 +0,0 @@
-class MySql::Driver < DB::Driver
-  def build_connection(context : DB::ConnectionContext)
-    MySql::Connection.new(context)
-  end
-end
-
-DB.register_driver "mysql", MySql::Driver
--- analysis/preprocessing/crystal_scripts/lib/mysql/src/mysql/version.cr
+++
@@ -1,3 +0,0 @@
-module MySql
-  VERSION = "0.6.0"
-end
